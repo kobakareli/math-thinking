7f0666dc3eafa31bacc06e8381e310a4226e47cf
kobakareli <kkare13@freeuni.edu.ge>
2018-07-16 22:11:16 +0400

    hide empty tasks and articles

--- website/resources/views/pages/article.blade.php
+++ website/resources/views/pages/article.blade.php
@@ -12,29 +12,31 @@
             {!! $article->{'text_' . App::getLocale()} !!}
         </div>
 
-        <div class="trending article-tasks">
-            <span class="blog-section-title fs-20">{{ trans('web.problems') }}</span>
-            @if($moretasks)
-                <a href="{{ url('/' . App::getLocale() . '/search/tasks/-1/' . $article->categories[0]->id) }}" class="see-more-link fs-20">{{ trans('web.see_more') }}</a>
-            @endif
-            <div class="trends">
-                @foreach($tasks as $ptask)
-                    <a href="{{ url('/' . App::getLocale() . '/task/' . $ptask->id) }}">
-                        <div class="trend">
+        @if(count($tasks) > 0)
+            <div class="trending article-tasks">
+                <span class="blog-section-title fs-20">{{ trans('web.problems') }}</span>
+                @if($moretasks)
+                    <a href="{{ url('/' . App::getLocale() . '/search/tasks/-1/' . $article->categories[0]->id) }}" class="see-more-link fs-20">{{ trans('web.see_more') }}</a>
+                @endif
+                <div class="trends">
+                    @foreach($tasks as $ptask)
+                        <a href="{{ url('/' . App::getLocale() . '/task/' . $ptask->id) }}">
+                            <div class="trend">
 
-                            <p class="title fs-16">
-                                {{ $ptask->{'title_' . App::getLocale()} }}
-                            </p>
-                            <div class="info">
-                                <span class="date fs-15">
-                                    {{ \Carbon\Carbon::parse(explode(" ",$ptask->created_at)[0])->format('d/m/Y') }}
-                                </span>
+                                <p class="title fs-16">
+                                    {{ $ptask->{'title_' . App::getLocale()} }}
+                                </p>
+                                <div class="info">
+                                    <span class="date fs-15">
+                                        {{ \Carbon\Carbon::parse(explode(" ",$ptask->created_at)[0])->format('d/m/Y') }}
+                                    </span>
+                                </div>
                             </div>
-                        </div>
-                    </a>
-                @endforeach
+                        </a>
+                    @endforeach
+                </div>
             </div>
-        </div>
+        @endif
 
         <div class="fb-comments" data-href="{{ url('/' . App::getLocale() . '/article/' . $article->id) }}" data-numposts="5"></div>
 
